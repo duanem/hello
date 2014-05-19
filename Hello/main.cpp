9f09117a820ab68aa88b6c4e37c19c9d0cb361c6
Duane Murphy <duanemurphy@mac.com>
2014-05-18 21:33:47 -0700

    Initial Commit

---
+++ Hello/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hello
+//
+//  Created by Duane Murphy on 5/18/14.
+//  Copyright (c) 2014 RedSeal Networks, Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
